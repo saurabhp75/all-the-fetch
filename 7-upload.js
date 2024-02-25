db6743ac66a9510fd6f1651b6b75210d0c5dfbf9
Saurabh Prakash <saurabhp75@gmail.com>
2024-02-25 10:20:38 +0530

    Upload json data from the browser using JS

--- 7-upload.js
+++ 7-upload.js
@@ -5,31 +5,37 @@
 let endpoint = 'http://127.0.0.1:3000/';
 
 export function setData() {
-  //
+  // Get the input elements for image and json
   const imgInput = document.getElementById('imgfile');
   const jsonInput = document.getElementById('jsonfile');
 
+  // Add submit event handler to form 
   document.getElementById('myform').addEventListener('submit', (ev) => {
     ev.preventDefault();
+
     //upload something
     let obj = {
       id: 123,
       name: 'steve',
     };
+
+    // Serialize/stringify the JS object
     let jsonstring = JSON.stringify(obj);
 
-    let fd = new FormData(document.getElementById('myform'));
+    // let fd = new FormData(document.getElementById('myform'));
     // console.log(imgInput.files[0]);
     // fd.append('imageFile', imgInput.files[0], imgInput.files[0].name);
 
     let request = new Request(endpoint, {
       method: 'POST',
-      // body: jsonstring,
-      body: fd,
+      body: jsonstring,
+      // body: fd,
       headers: {
-        'content-type': 'multipart/form-data',
+        // 'content-type': 'multipart/form-data',
+        'content-type': 'application/json'
       },
     });
+
     fetch(request)
       .then((response) => {
         if (!response.ok) throw new Error('invalid');
